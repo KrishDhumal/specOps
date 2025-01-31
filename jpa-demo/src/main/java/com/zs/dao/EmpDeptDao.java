80f2c44ffe2660c2de14d045e0490473f9d630b1
zubairify <41644191+zubairify@users.noreply.github.com>
2025-01-31 11:07:02 +0530

    JPA Updated

--- jpa-demo/src/main/java/com/zs/dao/EmpDeptDao.java
+++ jpa-demo/src/main/java/com/zs/dao/EmpDeptDao.java
@@ -13,42 +13,78 @@ import com.zs.entity.Employee;
 public class EmpDeptDao {
 
 	private EntityManagerFactory factory;
-	
+
 	public EmpDeptDao() {
 		factory = Persistence.createEntityManagerFactory("MyJPA");
 	}
-	
-	public void add(Object obj) {
+
+	public boolean add(Object obj) {
 		EntityManager em = factory.createEntityManager();
 		EntityTransaction txn = em.getTransaction();
-		
+
 		txn.begin();
 		try {
 			em.persist(obj);
 			txn.commit();
+			return true;
 		} catch (Exception e) {
 			txn.rollback();
 			e.printStackTrace();
+			return false;
 		}
 	}
-	
+
 	public Department getDept(int deptNo) {
 		EntityManager em = factory.createEntityManager();
 		return em.find(Department.class, deptNo);
 	}
-	
+
 	public Employee getEmp(int empNo) {
 		EntityManager em = factory.createEntityManager();
 		return em.find(Employee.class, empNo);
 	}
-	
+
 	public List<Department> allDept() {
 		EntityManager em = factory.createEntityManager();
 		return em.createQuery("FROM Department").getResultList();
 	}
-	
+
 	public List<Employee> allEmp() {
 		EntityManager em = factory.createEntityManager();
 		return em.createQuery("FROM Employee").getResultList();
+	}
+
+	public boolean delDept(int deptNo) {
+		EntityManager em = factory.createEntityManager();
+		EntityTransaction txn = em.getTransaction();
+
+		txn.begin();
+		try {
+			Department d = em.find(Department.class, deptNo);
+			em.remove(d);
+			txn.commit();
+			return true;
+		} catch (Exception e) {
+			txn.rollback();
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean delEmp(int empNo) {
+		EntityManager em = factory.createEntityManager();
+		EntityTransaction txn = em.getTransaction();
+
+		txn.begin();
+		try {
+			Employee e = em.find(Employee.class, empNo);
+			em.remove(e);
+			txn.commit();
+			return true;
+		} catch (Exception e) {
+			txn.rollback();
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
