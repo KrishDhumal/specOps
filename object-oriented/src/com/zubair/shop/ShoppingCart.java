b4bd4825ac0f56507fbaf127dcc48d63e78db4b1
krishdhumal <krishdhumal@localhost>
2025-04-18 21:47:54 +0530

    ,..,kkml

--- object-oriented/src/com/zubair/shop/ShoppingCart.java
+++ object-oriented/src/com/zubair/shop/ShoppingCart.java
@@ -1,29 +1,30 @@
 package com.zubair.shop;
 
-import java.util.ArrayList;
-
 public class ShoppingCart {
-	private ArrayList<Product> items;
-	private double total;
-	
-	public ShoppingCart() {
-		items = new ArrayList<Product>();
-	}
+    private Product[] items;
+    private int itemCount; // To keep track of how many items are added
+    private double total;
 
-	public void addItem(Product p) throws ShoppingCartException {
-		if(5 == items.size())
-			throw new ShoppingCartException("Shopping cart is full!!");
-		else {
-			items.add(p);
-			total += p.getPrice();
-		}
-	}
-	
-	public void checkout() {
-		for(Product p : items) 
-			p.print();
-		
-		System.out.println("Cart Total: " + total);
-	}
+    public ShoppingCart() {
+        items = new Product[5]; // Fixed size array
+        itemCount = 0;
+        total = 0.0;
+    }
+
+    public void addItem(Product p) throws ShoppingCartException {
+        if (itemCount == 5) {
+            throw new ShoppingCartException("Shopping cart is full!!");
+        } else {
+            items[itemCount] = p;
+            itemCount++;
+            total += p.getPrice();
+        }
+    }
+
+    public void checkout() {
+        for (int i = 0; i < itemCount; i++) {
+            items[i].print();
+        }
+        System.out.println("Cart Total: " + total);
+    }
 }
-
